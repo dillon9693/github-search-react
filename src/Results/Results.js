72009bca29687c92ddbf010f787e4294237a1c4d
Dillon <dillon.p.kerr@gmail.com>
2017-11-10 20:05:08 -0500

    Create a Results component containng an unordered list of strings

---
+++ src/Results/Results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Results = (props) => {
+  return (
+    <ul class="results-container">
+      <li>Test 1</li>
+      <li>Test 2</li>
+      <li>Test 3</li>
+      <li>Test 4</li>
+    </ul>
+  );
+};
+
+export default Results;
