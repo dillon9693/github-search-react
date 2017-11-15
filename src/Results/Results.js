4c6c9e2caf3e6a2b946ee58174893039f22ddda6
Dillon <dillon.p.kerr@gmail.com>
2017-11-14 22:56:47 -0500

    For each result passed to the Results component, displays a RepoResult component

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Results = (props) => {
+import RepoResult from '../RepoResult/RepoResult';
+
+const Results = ({ results }) => {
   return (
-    <ul class="results-container">
-      <li>Test 1</li>
-      <li>Test 2</li>
-      <li>Test 3</li>
-      <li>Test 4</li>
+    <ul className="results-container">
+      {
+        results.map((result) => {
+          return <RepoResult key={result.id} result={result} />
+        })
+      }
     </ul>
   );
 };
