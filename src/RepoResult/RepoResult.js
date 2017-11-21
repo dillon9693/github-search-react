3c8a559d9e1bc1b28cfc49a20eda7e8cbf8300bb
Dillon <dillon.p.kerr@gmail.com>
2017-11-21 18:16:42 -0500

    Change the results-container to be a div instead of a ul; change the repo results to be divs instead of list elements

--- src/RepoResult/RepoResult.js
+++ src/RepoResult/RepoResult.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const RepoResult = ({ result }) => {
   return (
-    <li className="repo-result">
+    <div className="repo-result">
       <a href={result.html_url} target="_blank">
         <div className="title">{result.name}</div>
         <div className="owner">{result.owner.login}</div>
         <div className="description">{result.description}</div>
       </a>
-    </li>
+    </div>
   );
 };
 
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -4,13 +4,13 @@ import RepoResult from '../RepoResult/RepoResult';
 
 const Results = ({ results }) => {
   return (
-    <ul className="results-container">
+    <div className="results-container">
       {
         results.map((result) => {
           return <RepoResult key={result.id} result={result} />
         })
       }
-    </ul>
+    </div>
   );
 };
 
