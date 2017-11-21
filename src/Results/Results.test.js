53bae499f687b8a7cbabbf32f9ffbf5170bd5913
Dillon <dillon.p.kerr@gmail.com>
2017-11-21 18:10:08 -0500

    Set the default results prop to be the empty array; add various tests for whether or not RepoResult elements are displayed based on how many results are passed in props

--- src/Results/Results.test.js
+++ src/Results/Results.test.js
@@ -28,7 +28,9 @@ describe('Results', () => {
   };
 
   beforeEach(() => {
-    props = {};
+    props = {
+      results: []
+    };
 
     shallowResults = undefined;
     mountedResults = undefined;
@@ -37,5 +39,29 @@ describe('Results', () => {
   it('should render a list', () => {
     var list = resultsShallow().find('ul');
     expect(list.length).toEqual(1);
+  });
+
+  it('should not render any RepoResult component', () => {
+    var repoResults = resultsShallow().find('RepoResult');
+    expect(repoResults.length).toEqual(0);
+  });
+
+  it('should render 1 RepoResult component', () => {
+    props.results = [
+      repositoryMock
+    ];
+
+    var repoResults = resultsShallow().find('RepoResult');
+    expect(repoResults.length).toEqual(1);
+  });
+
+  it('should render 5 RepoResult component', () => {
+    props.results = [
+      repositoryMock, repositoryMock, repositoryMock,
+      repositoryMock, repositoryMock
+    ];
+
+    var repoResults = resultsShallow().find('RepoResult');
+    expect(repoResults.length).toEqual(5);
   });
 });
