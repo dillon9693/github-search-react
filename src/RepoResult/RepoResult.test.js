38dbb26c757857327ed40e9d118236bf89cf099c
Dillon <dillon.p.kerr@gmail.com>
2017-11-21 18:27:50 -0500

    Add some test for the RepoResult component, one checking for the repo-result div, one checking that the div contains a link to the repository, and one checking that the title, owner, and description are being render from props

--- src/RepoResult/RepoResult.test.js
+++ src/RepoResult/RepoResult.test.js
@@ -34,4 +34,21 @@ describe('RepoResult', () => {
     shallowRepoResult = undefined;
     mountedRepoResult = undefined;
   });
+
+  it('should render a div with repo-result class', () => {
+    var div = repoResultShallow().find('.repo-result');
+    expect(div.length).toEqual(1);
+  });
+
+  it('should render a link to the repository', () => {
+    const link = repoResultShallow().find('.repo-result > a');
+    expect(link.length).toEqual(1);
+    expect(link.prop('href')).toEqual(repositoryMock.html_url);
+  });
+
+  it('should render a title, owner, and description from mock', () => {
+    expect(repoResultShallow().find('.title').text()).toEqual(repositoryMock.name);
+    expect(repoResultShallow().find('.owner').text()).toEqual(repositoryMock.owner.login);
+    expect(repoResultShallow().find('.description').text()).toEqual(repositoryMock.description);
+  });
 });
