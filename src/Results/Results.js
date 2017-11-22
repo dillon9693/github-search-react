938b29caf1c0d2fba0c55e9f01b5f5611202a38d
Dillon <dillon.p.kerr@gmail.com>
2017-11-21 22:02:51 -0500

    If the results passed to the Results component are empty then display a message indiciating that; otherwise, display the results

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import RepoResult from '../RepoResult/RepoResult';
 
-const Results = ({ results }) => {
+const Results = ({ results, searchTerm }) => {
   return (
     <div className="results-container">
       {
-        results.map((result) => {
-          return <RepoResult key={result.id} result={result} />
-        })
+        results.length > 0
+        ? results.map((result) => {
+            return <RepoResult key={result.id} result={result} />
+          })
+        : <div class="results-empty">No results found for "<span class="text-bold">{searchTerm}</span>".</div>
       }
     </div>
   );
