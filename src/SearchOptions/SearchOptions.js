183390d233ddedec6d018714694be6a692b44a18
Dillon <dillon.p.kerr@gmail.com>
2017-11-23 13:24:29 -0500

    Move all of the SearchOptions component's styles into a separate CSS file and import above the Component declaration

--- src/SearchOptions/SearchOptions.js
+++ src/SearchOptions/SearchOptions.js
@@ -4,52 +4,21 @@ import IconButton from 'material-ui/IconButton';
 import ArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
 import ArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 
-const styles = {
-  'search-options': {
-    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
-  },
-  'search-options-panel': {
-    maxHeight: '0',
-    overflow: 'auto',
-    padding: '0 10px',
-    transition: 'max-height 800ms ease-in-out 0ms'
-  },
-  'search-options-panel-open': {
-    maxHeight: '1000px'
-  },
-  'search-options-panel-content': {
-    padding: '10px 0'
-  },
-  'search-options-toggle-panel': {
-    backgroundColor: '#E8E8E8',
-    border: '1px solid transparent',
-    cursor: 'pointer',
-    display: 'flex',
-    height: '30px',
-    justifyContent: 'space-between',
-    padding: '0 12px'
-  },
-  'search-options-toggle-panel-content': {
-    alignItems: 'center',
-    display: 'flex',
-    position: 'relative',
-
-  }
-};
+import './SearchOptions.css';
 
 const SearchOptions = ({ handleToggle, open }) => {
-  const panelStyles =
+  const panelClasses =
     open
-    ? {...styles['search-options-panel'], ...styles['search-options-panel-open']}
-    : styles['search-options-panel'];
+    ? 'search-options-panel search-options-panel-open'
+    : 'search-options-panel';
 
   return (
-    <div style={styles['search-options']}>
+    <div className='search-options'>
       <div
         onClick={handleToggle}
-        style={styles['search-options-toggle-panel']}
+        className='search-options-toggle-panel'
       >
-        <div style={styles['search-options-toggle-panel-content']}>
+        <div className='search-options-toggle-panel-content'>
           <IconButton>
           { open
             ? <ArrowDown />
@@ -59,8 +28,8 @@ const SearchOptions = ({ handleToggle, open }) => {
           <span>Advanced Search</span>
         </div>
       </div>
-      <div style={panelStyles}>
-        <div style={styles['search-options-panel-content']}>
+      <div className={panelClasses}>
+        <div className='search-options-panel-content'>
           <div>Search Contents</div>
         </div>
       </div>
