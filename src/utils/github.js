2145c2888618baaa36db41a2f76e5170b76ef65c
Dillon <dillon.p.kerr@gmail.com>
2017-11-23 15:48:12 -0500

    Fix the default options argument for the searchRepositories function to be syntactically correct; add the page option

--- src/utils/github.js
+++ src/utils/github.js
@@ -2,7 +2,7 @@
  * Github API functions
  */
 
-export const searchRepositories = async (term, { sort: '', order: 'desc' }) => {
+export const searchRepositories = async (term, { sort = '', order = 'desc', page = 1 } = {}) => {
   if(!term) {
     return [];
   }
