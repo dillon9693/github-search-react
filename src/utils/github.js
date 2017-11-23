945c0abb2a2dcf6ca289e1a1ba8f3154397d1826
Dillon <dillon.p.kerr@gmail.com>
2017-11-23 15:49:21 -0500

    Import the get function from axios; hit the Github Search API with the passed parameters and return the results; if there's an error, return the empty array

--- src/utils/github.js
+++ src/utils/github.js
@@ -1,9 +1,19 @@
 /**
  * Github API functions
  */
+import { get } from 'axios';
 
 export const searchRepositories = async (term, { sort = '', order = 'desc', page = 1 } = {}) => {
   if(!term) {
+    return [];
+  }
+
+  try {
+    const response = await get(`${GITHUB_API_URL}/search/repositories?q=${term}&page=${page}&sort=${sort}&order=${order}`);
+
+    return response.data.items;
+  }
+  catch(e) {
     return [];
   }
 };
