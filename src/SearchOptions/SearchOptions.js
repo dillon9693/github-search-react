af95646a5a60436c1573d53a608e4b391a63debf
Dillon <dillon.p.kerr@gmail.com>
2017-11-23 13:03:20 -0500

    Add some markup for the SearchOptions component, containing a toggle bar (with material design styles) and a hidden panel that opens when the toggle bar is clicked

--- src/SearchOptions/SearchOptions.js
+++ src/SearchOptions/SearchOptions.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
+const styles = {
+  'search-options': {
+    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
+  },
+  'search-options-panel': {
+    maxHeight: '0',
+    overflow: 'auto',
+    padding: '0 10px',
+    transition: 'max-height 800ms ease-in-out 0ms'
+  },
+  'search-options-panel-content': {
+    padding: '10px 0'
+  },
+  'search-options-toggle-panel': {
+    backgroundColor: '#E8E8E8',
+    border: '1px solid transparent',
+    cursor: 'pointer',
+    display: 'flex',
+    height: '30px',
+    justifyContent: 'space-between',
+    padding: '0 12px'
+  },
+  'search-options-toggle-panel-content': {
+    alignItems: 'center',
+    display: 'flex',
+    position: 'relative',
+
+  }
+};
+
 const SearchOptions = (props) => {
   return (
-    <div>Search Options</div>
+    <div style={styles['search-options']}>
+      <div
+        style={styles['search-options-toggle-panel']}
+      >
+        <div style={styles['search-options-toggle-panel-content']}>
+          <span>Advanced Search</span>
+        </div>
+      </div>
+      <div style={styles['search-options-panel']}>
+        <div style={styles['search-options-panel-content']}>
+          <div>Search Contents</div>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default SearchOptions;
