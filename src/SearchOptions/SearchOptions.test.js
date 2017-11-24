6e24e37ab1c34a647fc693e832b50c2770eb479e
Dillon <dillon.p.kerr@gmail.com>
2017-11-23 20:29:08 -0500

    Add some tests for the SearchOptions component

--- src/SearchOptions/SearchOptions.test.js
+++ src/SearchOptions/SearchOptions.test.js
@@ -36,4 +36,38 @@ describe('SearchOptions', () => {
     shallowSearchOptions = undefined;
     mountedSearchOptions = undefined;
   });
+
+  it('renders a div with search-options class', () => {
+    expect(searchOptionsShallow().find('.search-options').length).toEqual(1);
+  });
+
+  it('renders a SelectField with 4 MenuItem children', () => {
+    const select = searchOptionsShallow().find('SelectField');
+    expect(select.length).toEqual(1);
+    const menuItems = select.find('MenuItem');
+    expect(menuItems.length).toEqual(4);
+
+    expect(menuItems.get(0).props.value).toEqual('');
+    expect(menuItems.get(1).props.value).toEqual('forks');
+    expect(menuItems.get(2).props.value).toEqual('stars');
+    expect(menuItems.get(3).props.value).toEqual('updated');
+  });
+
+  it('calls handleSortFilterChange function when the sort filter SelectField is changed', () => {
+    props.handleSortFilterChange = jest.fn();
+    const select = searchOptionsShallow().find('SelectField');
+    select.simulate('change');
+    expect(props.handleSortFilterChange.mock.calls.length).toEqual(1);
+  });
+
+  it('calls the handleToggle function when the toggle panel is clicked', () => {
+    props.handleToggle = jest.fn();
+    const togglePanel = searchOptionsShallow().find('.search-options-toggle-panel').simulate('click');
+    expect(props.handleToggle.mock.calls.length).toEqual(1);
+  });
+
+  it('sets the value of the SelectField when the sortFilter prop is set', () => {
+    props.sortFilter = 'forks';
+    expect(searchOptionsShallow().find('SelectField').prop('value')).toEqual('forks');
+  });
 });
