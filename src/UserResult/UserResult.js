4aa3e71b3ed6495947ec85e7ea9659e260b4aea6
Dillon <dillon.p.kerr@gmail.com>
2017-12-24 17:56:32 -0500

    Create a UserResult component that renders a result div containing info about a Github user

---
+++ src/UserResult/UserResult.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserResult = ({ result }) => {
+  return (
+    <div className="result user-result">
+      <a href={result.html_url} target="_blank">
+        <div className="avatar">
+          <img src={result.avatar_url} alt={`${result.login} avatar`} />
+        </div>
+        <div className="login">{result.login}</div>
+      </a>
+    </div>
+  );
+};
+
+export default UserResult;
