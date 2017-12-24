b415afbf82b270230f67a1c82fb8d1429ff562b9
Dillon <dillon.p.kerr@gmail.com>
2017-12-24 17:58:29 -0500

    Add tests for the UserResult component

---
+++ src/UserResult/UserResult.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import UserResult from './UserResult';
+
+import userMock from '../__mocks__/userMock';
+
+describe('UserResult', () => {
+  let props;
+  let shallowUserResult;
+  let mountedUserResult;
+
+  const userResultShallow = () => {
+    if(!shallowUserResult) {
+      shallowUserResult = shallow(
+        <UserResult {...props} />
+      );
+    }
+    return shallowUserResult;
+  };
+
+  const userResultMounted = () => {
+    if(!mountedRepoResut) {
+      mountedUserResult = mount(
+        <UserResult {...props} />
+      );
+    }    return mountedUserResult;
+  };
+
+  beforeEach(() => {
+    props = {
+      result: userMock
+    };
+
+    shallowUserResult = undefined;
+    mountedUserResult = undefined;
+  });
+
+  it('should render a div with user-result class', () => {
+    var div = userResultShallow().find('.user-result');
+    expect(div.length).toEqual(1);
+  });
+
+  it('should render a link to the repository', () => {
+    const link = userResultShallow().find('.user-result > a');
+    expect(link.length).toEqual(1);
+    expect(link.prop('href')).toEqual(userMock.html_url);
+  });
+
+  it('should render an avatar image and login from mock', () => {
+    expect(userResultShallow().find('.avatar > img').prop('src')).toEqual(userMock.avatar_url);
+    expect(userResultShallow().find('.login').text()).toEqual(userMock.login);
+  });
+});
