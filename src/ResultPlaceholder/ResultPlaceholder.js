c0b491d311092fc19ab446151d9a57c99924dab6
Dillon <dkerr@mindedge.ocm>
2017-12-28 15:02:20 -0500

    Display various placeholder elements in the placeholder based on the type parameter that is passed as a prop to the component

--- src/ResultPlaceholder/ResultPlaceholder.js
+++ src/ResultPlaceholder/ResultPlaceholder.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { TextBlock } from 'react-placeholder/lib/placeholders';
+import { RectShape, TextBlock } from 'react-placeholder/lib/placeholders';
 
 import './ResultPlaceholder.css';
 
 const RepoResultPlaceholder = ({ type }) => (
   <div className={`result ${type}-result`}>
     <a href="javascript:void(0);">
-      <TextBlock rows={3} color='lightgray' />
+      {
+        type === 'users'
+        ? <RectShape color='lightgray' style={{ width: 50, height: 50 }} />
+        : null
+      }
+      <TextBlock rows={type === 'users' ? 1 : 3} color='lightgray' />
     </a>
   </div>
 );
