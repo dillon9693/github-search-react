8b8ab2a4f6135d291c16923cbbe2add4852391f2
Dillon <dkerr@mindedge.ocm>
2017-12-28 15:15:33 -0500

    Add some additional tests for the ResultPlaceholder component that test what should be displayed based on the type prop

--- src/ResultPlaceholder/ResultPlaceholder.test.js
+++ src/ResultPlaceholder/ResultPlaceholder.test.js
@@ -23,10 +23,17 @@ describe('ResultPlaceholder', () => {
     shallowResultPlaceholder = undefined;
   });
 
-  it('should render a div with repositories-result class', () => {
+  it('should render a div with repositories-result class when type is \'repositories\'', () => {
     var div = resultShallow();
     expect(div.hasClass('result')).toEqual(true);
     expect(div.hasClass('repositories-result')).toEqual(true);
+  });
+
+  it('should render a div with users-result class when type is \'users\'', () => {
+    props.type = 'users';
+    var div = resultShallow();
+    expect(div.hasClass('result')).toEqual(true);
+    expect(div.hasClass('users-result')).toEqual(true);
   });
 
   it('should render an anchor tag with href=\'javascript:void(0);\'', () => {
@@ -36,4 +43,29 @@ describe('ResultPlaceholder', () => {
   it('should render a TextBlock component', () => {
     expect(resultShallow().find('a > TextBlock').length).toEqual(1);
   })
+
+  it('should render a TextBlock component with rows prop set to 3 when type is \'repositories\'', () => {
+    const textBlock = resultShallow().find('a > TextBlock');
+    expect(textBlock.length).toEqual(1);
+    expect(textBlock.prop('rows')).toEqual(3);
+  });
+
+  it('should render a TextBlock component with rows prop set to 1 when type is \'users\'', () => {
+    props.type = 'users';
+    const textBlock = resultShallow().find('a > TextBlock');
+    expect(textBlock.length).toEqual(1);
+    expect(textBlock.prop('rows')).toEqual(1);
+  });
+
+  it('should not render a RectShape component when type is \'repositories\'', () => {
+    const rectShape = resultShallow().find('a > RectShape');
+    expect(rectShape.length).toEqual(0);
+  });
+
+  it('should render a RectShape component when type is \'users\'', () => {
+    props.type = 'users';
+    const rectShape = resultShallow().find('a > RectShape');
+    expect(rectShape.length).toEqual(1);
+  });
+
 });
