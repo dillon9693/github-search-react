b8c877cbf0af381dd8d6559213066aed946cc42a
Dillon <dkerr@mindedge.ocm>
2017-12-28 11:00:06 -0500

    Use the react-placeholder package to creat a RepoResultPlaceholder component with styles and a test file

---
+++ src/RepoResultPlaceholder/RepoResultPlaceholder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TextBlock } from 'react-placeholder/lib/placeholders';
+
+import './RepoResultPlaceholder.css';
+
+const RepoResultPlaceholder = () => (
+  <div className='result repo-result'>
+    <a href="javascript:void(0)">
+      <TextBlock rows={3} color='lightgray' />
+    </a>
+  </div>
+);
+
+export default RepoResultPlaceholder;
